be72041d6f46cf06206f9557f05d54f522735e7b
philornot <philornot@users.noreply.github.com>
2025-10-07 11:29:00 +0200

    liczba pierwsza

---
+++ czy_pierwsza.py
@@ -0,0 +1,14 @@
+def czy_pierwsza(liczba):
+    pierwsza = False
+    i = 2
+    while i >= pierwsza * 0.5:
+        if liczba % i == 0:
+            return False
+        i += 1
+    if liczba >= 2:
+        return True
+    else:
+        return False
+
+
+print(czy_pierwsza(64))
