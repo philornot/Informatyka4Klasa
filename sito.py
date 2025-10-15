cf2598e0d703989b20de32ab8d7e6ef0dfb344b6
Filip <philornot3@gmail.com>
2025-10-15 17:11:47 +0200

    Poprawki sita Erakogośtam
    Updated condition and comments for clarity and optimization.

--- sito.py
+++ sito.py
@@ -8,9 +8,10 @@ while i < n:
 
 i = 2
 while i < n:
-    if i == 0:
+    if tab[i] == 0:
         continue
-    x = i * i  # czy tu powinno być * 2? bo tak też działą
+    x = i * i  # potęgowanie optymalizuje kod (zaczynamy od potęgi liczby pierwszej, wszystkie mniejsze zostały już skreślone przez mniejsze liczby pierwsze)
+    # czyli dla i = 3, x = 9, później 12, 15... -> liczby podzielne przez 3 i < 2 (czyli 6) zostały już 'skreślone' przez dwójkę.
     # print(f'i={i}, x ={x}, n={n}')
     while x < n:
         tab[x] = 0
