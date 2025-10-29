74c21e413462005ba80781f34a2361232ff3a67d
philornot <philornot@users.noreply.github.com>
2025-10-29 09:34:58 +0100

    na pewno ok

--- wyszukiwanie_bin.py
+++ wyszukiwanie_bin.py
@@ -6,20 +6,18 @@ def szukaj_parzystej(tab):
     while True:
         if tab[srodek] % 2 == 0:
             prawy = srodek - 1
+            if tab[srodek-1] % 2 != 0:
+                return tab[srodek]
             srodek = (lewy + prawy) // 2
-            if tab[lewy] % 2 == 0:
-                return tab[lewy]
+
         else:
             lewy = srodek + 1
             srodek = (lewy + prawy) // 2
-            if tab[lewy] % 2 == 0:
-                return tab[lewy - 1]
 
 
 tab = [1, 23, 43, 31, 21, 47, 18, 22, 12, 6, 24]
 tab2 = [1, 231, 14, 12, 44, 24, 12]
 tab3 = [1, 231, 15, 13, 45, 23, 12]
-print(tab)
-print(szukaj_parzystej(tab))
-print(szukaj_parzystej(tab2))
-print(szukaj_parzystej(tab3))
+print(tab, szukaj_parzystej(tab))
+print(tab2, szukaj_parzystej(tab2))
+print(tab3, szukaj_parzystej(tab3))
