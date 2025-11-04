18135a2f5bb1e0805dff4fbe5d1f009bc334a8d8
philornot <philornot@users.noreply.github.com>
2025-11-04 11:26:38 +0100

    szacowanie dla 35 z liczeniem iteracji

--- newton-raphson.py
+++ newton-raphson.py
@@ -2,14 +2,16 @@ def oszacuj_pierwiastek(pierwiastek_do_kwadratu, dokladnosc=0.00001):
     pole_kwadratu = pierwiastek_do_kwadratu
     a = 1
     b = pole_kwadratu
+    licznik = 0
     while abs(a - b) >= dokladnosc:
         a = (a + b) / 2
         b = pole_kwadratu / a
-        # print(a, b)
+        licznik += 1
 
-    return (a + b) / 2
+    wynik = (a + b) / 2
+    return wynik, licznik
 
 
-for i in range(15):
-    oszacowany_pierw = oszacuj_pierwiastek(i + 1)
-    print(f'Pierwiastek z {i + 1}: {oszacowany_pierw}')
+c = 35
+oszacowany_pierw, wykonania = oszacuj_pierwiastek(c, dokladnosc=0.0001)
+print(f'Pierwiastek z {c}: {oszacowany_pierw}, było {wykonania} wykonań funkcji.')
